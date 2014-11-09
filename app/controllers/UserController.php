ba70718ad0c2412f27a3ee9c13bbc1cc94aa7222
YuxiangRose <ddgithub@gmail.com>
2014-11-08 17:22:42 -0800

    add assets floder

---
+++ app/controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+class HomeController extends BaseController {
+
+    public function getIndex()
+    {
+        $this->postLogin();
+    }
+
+   
+    public function getLogin()
+    {
+        
+    }
+
+    public function postLogin(){
+        
+    }
+
+}
+
+?>
---
+++ app/views/layouts/css.blade.php
@@ -0,0 +1,29 @@
+
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+{{ HTML::style('http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all');}}
+{{ HTML::style('assets/global/plugins/font-awesome/css/font-awesome.min.css'); }}
+{{ HTML::style('assets/global/plugins/simple-line-icons/simple-line-icons.min.css'); }}
+{{ HTML::style('assets/global/plugins/bootstrap/css/bootstrap.min.css'); }}
+{{ HTML::style('assets/global/plugins/uniform/css/uniform.default.css'); }}
+{{ HTML::style('assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css'); }}
+<!-- END GLOBAL MANDATORY STYLES -->
+
+<!-- BEGIN PAGE LEVEL PLUGIN STYLES -->
+{{ HTML::style('assets/global/plugins/gritter/css/jquery.gritter.css'); }}
+{{ HTML::style('assets/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css'); }}
+{{ HTML::style('assets/global/plugins/fullcalendar/fullcalendar/fullcalendar.css'); }}
+{{ HTML::style('assets/global/plugins/jqvmap/jqvmap/jqvmap.css'); }}
+<!-- END PAGE LEVEL PLUGIN STYLES -->
+
+<!-- BEGIN PAGE STYLES -->
+{{ HTML::style('assets/admin/pages/css/tasks.css'); }}
+<!-- END PAGE STYLES -->
+
+<!-- BEGIN THEME STYLES -->
+{{ HTML::style('assets/global/css/components.css'); }}
+{{ HTML::style('assets/global/css/plugins.css'); }}
+{{ HTML::style('assets/admin/layout/css/layout.css'); }}
+{{ HTML::style('assets/admin/layout/css/themes/default.css',array('id'=>'style_color')); }}
+{{ HTML::style('assets/admin/layout/css/custom.css'); }}
+<!-- END THEME STYLES -->
+
