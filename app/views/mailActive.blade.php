9b70d29850d0eb2d51ea3df16f060b7a70d8fb30
YuxiangRose <ddgithub@gmail.com>
2015-01-25 16:49:51 -0800

    mailactive view css style changed

--- app/views/mailActive.blade.php
+++ app/views/mailActive.blade.php
@@ -11,10 +11,83 @@
 		<div class="page-content-wrapper">
 			<div class="page-content" style="min-height:1023px">
 				{{ $mailActiveContent }}
-				<form action="{{URL::action('HomeController@getMailResend')}}" id="email-resubmission">
-					<input type="text" name="email" type="email" placeholder="Email">
-					<input type="submit">
-				</form>
+				<!-- <div class="row">
+					<div class="col-md-6">
+					<div class="portlet box blue">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-gift"></i>Notifycation 
+							</div>
+						</div>	
+						<div class="portlet-body form" style="display: block;">
+							<form role="form" action="{{URL::action('HomeController@getMailResend')}}" id="email-resubmission">
+								<div class="form-body">
+									<div class="input-group">
+										<span class="input-group-addon">
+										<i class="fa fa-envelope"></i>
+										</span>
+										<input class="form-control" type="text" name="email" placeholder="Email Address">
+									</div>
+								</div>
+								<div class="form-actions right">
+									<button class="btn blue" type="submit">Resend Email</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				
+					</div>						
+				</div> -->
+
+
+
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="portlet box blue">
+							<div class="portlet-title">
+							<div class="caption">
+							<i class="fa fa-gift"></i>
+							Default Form
+							</div>
+							<div class="tools">
+							<a class="collapse" href=""> </a>
+							<a class="config" data-toggle="modal" href="#portlet-config"> </a>
+							<a class="reload" href=""> </a>
+							<a class="remove" href=""> </a>
+							</div>
+							</div>
+							<div class="portlet-body form">
+							<form role="form">
+							<div class="form-body">
+							<div class="form-group">
+							<label>Email Address</label>
+							<div class="input-group">
+							<span class="input-group-addon">
+							<i class="fa fa-envelope"></i>
+							</span>
+							<input class="form-control" type="text" placeholder="Email Address">
+							</div>
+							</div>
+							</div>
+							<div class="form-actions">
+							<button class="btn blue" type="submit">Submit</button>
+							<button class="btn default" type="button">Cancel</button>
+							</div>
+							</form>
+							</div>
+							</div>
+					</div>
+				</div>
+
+
+
+
+
+
+
+
+				
 			</div>
 		</div>
 	</div>
