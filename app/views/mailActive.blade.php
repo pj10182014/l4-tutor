abfd51ad7fec382d556bfbf73fe694b8c2b0cc89
ET-GATE-BASH <et1103@hotmail.com>
2015-02-01 14:17:19 -0800

    fixed validation

--- app/views/mailActive.blade.php
+++ app/views/mailActive.blade.php
@@ -51,3 +51,27 @@
 		</div> <!-- end page-content-wraper -->
 	</div> <!-- end page-container -->
 @stop
+
+@section('js')
+@include('layouts.js.js-core-plugin')
+@include('layouts.js.js-login-page-level-plugin')
+@include('layouts.js.js-page-level-plugin')
+@include('layouts.js.js-page-level-script')
+ <script>
+$(document).ready(function(){
+	$("#email-resubmission").validate({
+		rules: {
+			email: {
+				required: true,
+				email: true
+			}
+		}
+	});
+
+	Metronic.init(); // init metronic core components
+	Layout.init(); // init current layout
+	QuickSidebar.init(); // init quick sidebar
+	
+});
+</script>
+@stop
