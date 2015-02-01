dacda282c912a29f9c98cb3e5f8e33b580d38443
YuxiangRose <ddgithub@gmail.com>
2015-02-01 13:57:51 -0800

    old version

--- app/views/mailActive.blade.php
+++ app/views/mailActive.blade.php
@@ -94,26 +94,3 @@
 		</div>
 	</div>
 @stop
-
-@section('js')
-@include('layouts.js.js-core-plugin')
-@include('layouts.js.js-login-page-level-plugin')
-@include('layouts.js.js-page-level-plugin')
-@include('layouts.js.js-page-level-script')
-<script>
-	$(document).ready(function(){
-		$("#email-resubmission").validate({
-			rules: {
-				email: {
-					required: true,
-					email: true
-				}
-			}
-		});
-
-		Metronic.init(); // init metronic core components
-		Layout.init(); // init current layout
-		QuickSidebar.init(); // init quick sidebar
-	});
-</script>
-@stop
