685fdb358c82d4fe22d9e04b3ba2b62db7b7b1cf
YuxiangRose <ddgithub@gmail.com>
2015-04-26 16:29:59 -0700

    update jquery to latest version

--- app/views/layouts/js/global/global.blade.php
+++ app/views/layouts/js/global/global.blade.php
@@ -5,7 +5,7 @@
 {{ HTML::script('assets/global/plugins/excanvas.min.js'); }}
 <![endif]-->
 
-{{ HTML::script('assets/global/plugins/jquery-1.11.0.min.js'); }}
+{{ HTML::script('assets/global/plugins/jquery-1.11.2.min.js'); }}
 {{ HTML::script('assets/global/plugins/jquery-migrate-1.2.1.min.js'); }}
 {{ HTML::script('assets/global/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js'); }}
 {{ HTML::script('assets/global/plugins/bootstrap/js/bootstrap.min.js'); }}
