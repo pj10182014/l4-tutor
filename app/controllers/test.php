0b8be8c0e6430bd9146c6c31407dedf8e126112f
ET-GATE-BASH <et1103@hotmail.com>
2015-05-17 15:56:00 -0700

    script select DB

--- app/controllers/test.php
+++ app/controllers/test.php
@@ -1,3 +1,26 @@
 <?php
-echo "fsdf";
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "tutor";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "SELECT * FROM users";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "id: " . $row["user_id"]. "user: " . $row["user_name"] . "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$conn->close();
 ?>
