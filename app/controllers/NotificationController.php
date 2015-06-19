3cce7960ace557db5afd59bb130c8392e1c052dd
YuxiangRose <ddgithub@gmail.com>
2015-06-19 16:55:25 -0700

    notification controller saved again

--- app/controllers/NotificationController.php
+++ app/controllers/NotificationController.php
@@ -6,6 +6,17 @@
                         $m->to("dpan218@gmail.com", "pan")->subject('Activation Email');}
                       );
 			echo count(Mail::failures());
+
+			make a notification model to get information from database;
+			Select * from notification table where status != 1 && resend <= 4;
+			send email;
+			if(count mail failures = 0){
+				update the notification table stauts;
+			}else{
+				update the notification tabel resend;
+			}
+
+
 		}
 	}
 ?>
